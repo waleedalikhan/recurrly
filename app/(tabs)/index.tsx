ce1cbc3f84e44f077cf0a14a2c47cbe1a63a7fab
Waleed Ali Khan <waleedomore@gmail.com>
2026-04-23 12:09:00 +0500

    implemented font typography

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -7,9 +8,25 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Recurily!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Link
+        href="/onboarding"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign Up
+      </Link>
     </SafeAreaView>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,13 @@
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Onboarding: React.FC = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-7xl font-sans-extrabold">Onboarding</Text>
+    </SafeAreaView>
+  );
+};
